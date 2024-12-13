7d1b0f3c85cbfe1ab8304d77383ce8c51219702e
brandontylerhall <bran.tyler.hall@gmail.com>
2024-12-13 15:26:56 -0500

    updated take and look_obj to delete the item you take and prevent the user from continuously looking at that object since it doesn't exist anymore

--- user_action.py
+++ user_action.py
@@ -1,3 +1,4 @@
+import random
 from PIL import Image
 
 
@@ -10,14 +11,13 @@ def handle_take(noun, current_room, rooms, game_state):
         inventory.append(rooms[current_room]["item"])
         print(f'You take the {noun}.')
         del rooms[current_room]["item"]
+        del rooms[current_room]['object'][noun]
     else:
         print(f'You look around and you don\'t see one of those.')
 
 
 def handle_use(noun, current_room, game_state):
     inventory = game_state["inventory"]
-    print(noun)
-    print(current_room)
 
     if noun == "pickaxe" and current_room == 'The Point of No Return':
         if 'pickaxe' in inventory:
@@ -33,25 +33,10 @@ def handle_look_around(current_room, rooms):
     print(rooms[current_room]['object']["around"])
 
 
-def handle_look_obj(noun, current_room, rooms, game_state):
-    try:
-        # Use the game_state dictionary to check if the items have been delivered
-        if noun in rooms[current_room]["object"]:
-            if noun == 'bed':
-                # Check if all items have been delivered using the game_state dictionary
-                if not game_state["items_delivered"]:
-                    print('The bed looks mad comfy but it isn\'t time for sleep yet! '
-                          'I still need to get dad his beer.')
-                else:
-                    print(rooms[current_room]["object"][noun])
-            elif noun in ['cigar case', 'box']:
-                if game_state.get('cigar_case_unlocked', False):
-                    print(rooms[current_room]["object"][noun]['unlocked'])
-                else:
-                    print(rooms[current_room]["object"][noun]['locked'])
-            else:
-                print(rooms[current_room]["object"][noun])
-    except KeyError:
+def handle_look_obj(noun, current_room, rooms):
+    if noun in rooms[current_room]["object"]:
+        print(rooms[current_room]["object"][noun])
+    else:
         print(f"I don't see a {noun} to look at.")
 
 
@@ -59,8 +44,54 @@ def handle_go(noun, rooms, game_state, current_room):
     if noun == '':
         print("You need to be more specific.")
     elif noun in rooms[current_room].keys():
-        game_state['current_room'] = rooms[current_room][noun]
+        new_room = rooms[current_room][noun]
+
+        # Check for pickaxe if entering Hidden Chamber
+        if new_room == 'Hidden Chamber' and not game_state['destroyed_wall']:
+            if 'pickaxe' not in game_state['inventory']:
+                print("You need a tool to break through this wall!")
+                return
+
+        # Check for required items if entering The Blighted Sepulcher
+        if new_room == 'The Blighted Sepulcher':
+            required_items = ['helm', 'brassard', 'ward', 'gauntlets', 'spear', 'boots', 'amulet']
+            missing_items = [item for item in required_items if item not in game_state['inventory']]
+            if missing_items:
+                if len(missing_items) > 1:
+                    missing_item = random.choice(missing_items)
+                    print(rooms[new_room]['object']['around'][f'no_{missing_item}'])
+                else:
+                    missing_item = missing_items[0]
+                    print(rooms[new_room]['object']['around'][f'no_{missing_item}'])
+                print('oh dear youre dead')
+                return
+
+        # Update current room in game_state
+        game_state['current_room'] = new_room
+
+        # Update conditional lore text
+        if 'object' in rooms[new_room]:
+            for obj_name, obj_data in rooms[new_room]['object'].items():
+                if isinstance(obj_data, dict) and any(
+                        key.startswith('before_') or key.startswith('after_') for key in obj_data.keys()):
+                    # Determine the correct key based on game state or inventory
+                    condition_key = None
+                    if obj_name == 'around' and new_room == 'Shadowed Alcove':
+                        condition_key = 'before_take_map' if 'map' not in game_state['inventory'] else 'after_take_map'
+                    elif obj_name == 'around' and new_room == 'The Point of No Return':
+                        condition_key = 'before_break_wall' if not game_state['destroyed_wall'] else 'after_break_wall'
+                    elif obj_name == 'altar' and new_room in ['Guthan\'s Tomb', 'Torag\'s Tomb', 'Verac\'s Tomb',
+                                                              'Dharok\'s Tomb', 'Karil\'s Tomb', 'Ahrim\'s Tomb']:
+                        item_name = rooms[new_room].get('item')
+                        condition_key = f'before_take_{item_name}' if item_name not in game_state[
+                            'inventory'] else f'after_take_{item_name}'
+
+                    # Print the appropriate description
+                    if condition_key:
+                        print(obj_data[condition_key])
+
         print(rooms[game_state['current_room']]['description'])
+
     else:
         print('I can\'t go that way.')
 
